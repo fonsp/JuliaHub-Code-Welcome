b9860be00e0c03cb970ff6db4b32b3d8ed1a7e82
Sebastian Pfitzner <pfitzseb@gmail.com>
2021-02-25 21:34:28 +0100

    shutdown warner

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,37 @@
 import * as vscode from 'vscode'
 import * as welcome from './welcome'
 
+const shutdownWarnInterval = 30000 // ms
+let shutdownWarner: NodeJS.Timeout|undefined = undefined
+
 export function activate(context: vscode.ExtensionContext) {
     welcome.activate(context)
+    shutdownWarner = setInterval(shutdownWarn, shutdownWarnInterval)
 }
 
-export function deactivate() {}
+const thresholds = [30, 15, 10, 5, 4, 3, 2, 1]
+function shutdownWarn() {
+    const stopTimeStamp = process.env['JH_JOB_STOP_TIME']
+    if (stopTimeStamp) {
+        const diffInMinutes = (Date.parse(stopTimeStamp) - Date.now())/1000/60
+        if (diffInMinutes < 0 && shutdownWarner) {
+            clearInterval(shutdownWarner)
+        }
+        let timeout = 0
+        for (const threshold of thresholds.reverse()) {
+            if (diffInMinutes < threshold) {
+                timeout = threshold
+                break
+            }
+        }
+
+        // TODO: would be nice to add a button here to extend the time limit:
+        vscode.window.showWarningMessage(`This session will time out in ${timeout} minutes. Make sure to backup your work or extend the time limit on JuliaHub.`)
+    }
+}
+
+export function deactivate() {
+    if (shutdownWarner) {
+        clearInterval(shutdownWarner)
+    }
+}
